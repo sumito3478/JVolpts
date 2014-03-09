006ccb4a1e5e2e11feffce51f79d08f16da7ab7c
sumito3478 <sumito3478@gmail.com>
2014-03-09 13:09:07 +0900

    Format AST.java

--- src/main/java/volpts/ast/AST.java
+++ src/main/java/volpts/ast/AST.java
@@ -4,149 +4,186 @@ import lombok.Value;
 import java.util.List;
 
 public interface AST {
-  @Value public static class Identifier implements AST {
+  @Value
+  public static class Identifier implements AST {
     String name;
   }
-  @Value public static class QualifiedIdentifier implements AST {
+  @Value
+  public static class QualifiedIdentifier implements AST {
     List<Identifier> ids;
   }
-  @Value public static class GenericIdentifier implements AST {
+  @Value
+  public static class GenericIdentifier implements AST {
     String name;
   }
-  @Value public static class Operator implements AST {
+  @Value
+  public static class Operator implements AST {
     String name;
   }
   public static interface Literal<A> extends AST {
     A getValue();
   }
-  @Value public static class BooleanLiteral implements Literal<Boolean> {
+  @Value
+  public static class BooleanLiteral implements Literal<Boolean> {
     Boolean value;
   }
-  @Value public static class IntegerLiteral implements Literal<Integer> {
+  @Value
+  public static class IntegerLiteral implements Literal<Integer> {
     Integer value;
   }
-  @Value public static class LongLiteral implements Literal<Long> {
+  @Value
+  public static class LongLiteral implements Literal<Long> {
     Long value;
   }
-  @Value public static class FloatLiteral implements Literal<Float> {
+  @Value
+  public static class FloatLiteral implements Literal<Float> {
     Float value;
   }
-  @Value public static class DoubleLiteral implements Literal<Double> {
+  @Value
+  public static class DoubleLiteral implements Literal<Double> {
     Double value;
   }
-  @Value public static class StringLiteral implements Literal<String> {
+  @Value
+  public static class StringLiteral implements Literal<String> {
     String value;
   }
-  @Value public static class Partial {
-    
+  @Value
+  public static class Partial {
+
   }
-  @Value public static class RecordPart {
+  @Value
+  public static class RecordPart {
     Identifier id;
     Expression name;
   }
-  @Value public static class VariantPart {
+  @Value
+  public static class VariantPart {
     Identifier id;
     Type type;
   }
-  @Value public static class MatchPart {
+  @Value
+  public static class MatchPart {
     Identifier id;
     List<Identifier> params;
     Expression cond;
     Expression expression;
   }
   public static interface Expression extends AST {
-    
+
   }
-  @Value public static class IdentifierExpression implements Expression {
+  @Value
+  public static class IdentifierExpression implements Expression {
     Identifier value;
   }
-  @Value public static class LiteralExpression implements Expression {
+  @Value
+  public static class LiteralExpression implements Expression {
     Literal<?> value;
   }
-  @Value public static class DotExpression implements Expression {
+  @Value
+  public static class DotExpression implements Expression {
     Expression lhs;
     Identifier rhs;
   }
-  @Value public static class UnaryExpression implements Expression {
+  @Value
+  public static class UnaryExpression implements Expression {
     Operator op;
     Expression expression;
   }
-  @Value public static class ApplicationExpression implements Expression {
+  @Value
+  public static class ApplicationExpression implements Expression {
     Expression lhs;
     Expression rhs;
   }
-  @Value public static class OperatorExpression implements Expression {
+  @Value
+  public static class OperatorExpression implements Expression {
     Expression lhs;
     Operator op;
     Expression rhs;
   }
-  @Value public static class IfExpression implements Expression {
+  @Value
+  public static class IfExpression implements Expression {
     Expression cond;
     Expression lhs;
     Expression rhs;
   }
-  @Value public static class LambdaExpression implements Expression {
+  @Value
+  public static class LambdaExpression implements Expression {
     Identifier id;
     Expression exp;
   }
-  @Value public static class PartialExpression implements Expression {
+  @Value
+  public static class PartialExpression implements Expression {
     Partial partial;
   }
-  @Value public static class LetRecExpression implements Expression {
+  @Value
+  public static class LetRecExpression implements Expression {
     Identifier id;
     Expression lhs;
     Expression rhs;
   }
-  @Value public static class LetExpression implements Expression {
+  @Value
+  public static class LetExpression implements Expression {
     Identifier id;
     Expression lhs;
     Expression rhs;
   }
-  @Value public static class DefExpression implements Expression {
+  @Value
+  public static class DefExpression implements Expression {
     Identifier id;
     Expression lhs;
     Expression rhs;
   }
-  @Value public static class RecordExpression implements Expression {
+  @Value
+  public static class RecordExpression implements Expression {
     List<RecordPart> parts;
   }
-  @Value public static class CompoundExpression implements Expression {
+  @Value
+  public static class CompoundExpression implements Expression {
     List<Expression> expressions;
   }
-  @Value public static class InlineExpression implements Expression {
+  @Value
+  public static class InlineExpression implements Expression {
     Identifier id;
     String text;
   }
-  @Value public static class VariantExpression implements Expression {
+  @Value
+  public static class VariantExpression implements Expression {
     Identifier id;
     List<VariantPart> parts;
     Expression expression;
   }
-  @Value public static class MatchExpression implements Expression {
+  @Value
+  public static class MatchExpression implements Expression {
     Expression expression;
     List<MatchPart> parts;
   }
   public static interface Type extends AST {
   }
-  @Value public static class IdentifierType implements Type {
+  @Value
+  public static class IdentifierType implements Type {
     QualifiedIdentifier id;
   }
-  @Value public static class GenericType implements Type {
+  @Value
+  public static class GenericType implements Type {
     GenericIdentifier id;
   }
-  @Value public static class TupleType implements Type {
+  @Value
+  public static class TupleType implements Type {
     Type lhs;
     Type rhs;
   }
-  @Value public static class FunctionType implements Type {
+  @Value
+  public static class FunctionType implements Type {
     Type lhs;
     Type rhs;
   }
-  @Value public static class RecordTypePart {
+  @Value
+  public static class RecordTypePart {
     Identifier name;
     Type type;
   }
-  @Value public static class RecordType implements Type {
+  @Value
+  public static class RecordType implements Type {
     List<RecordTypePart> parts;
   }
 //  @Value public static class ImportExpression implements Expression {

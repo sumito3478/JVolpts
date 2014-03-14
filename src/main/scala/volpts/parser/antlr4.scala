1e8174bf755410402bb911a36957e5c2b6206d43
sumito3478 <sumito3478@gmail.com>
2014-03-14 22:45:37 +0900

    Let ParserUtil.seq return Vector

--- src/main/scala/volpts/parser/antlr4.scala
+++ src/main/scala/volpts/parser/antlr4.scala
@@ -7,7 +7,7 @@ import scala.collection._
 class ParserUtils {
   def seq[A](xs: java.util.List[A]): Seq[A] = {
     import JavaConversions._
-    xs.toSeq
+    xs.toVector
   }
   def option[A](x: A): Option[A] = Option(x)
 }
